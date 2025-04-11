293188902ef6f913ab5db4ed3f2b436feeeaca9c
Mononoke Hime <mononoke@themake.rs>
2025-04-11 03:26:11 +0300

    fixed imports

--- ziiim_11/nikiforova/lesson_1_part_2/tasks_3_1/task_1_test.py
+++ ziiim_11/nikiforova/lesson_1_part_2/tasks_3_1/task_1_test.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-import task_3_1
+import task_1
 
 # Тест 1: D < 0
 print("Тест 1: a=1, b=5, c=10")
-task_3_1.solve_quadratic(1, 5, 10)
+task_1.solve_quadratic(1, 5, 10)
 print()
 
 # Тест 2: D > 0
 print("Тест 2: a=2, b=6, c=-1")
-task_3_1.solve_quadratic(2, 6, -1)
+task_1.solve_quadratic(2, 6, -1)
 print()
 
 # Тест 3: D == 0
 print("Тест 3: a=1, b=2, c=1")
-task_3_1.solve_quadratic(1, 2, 1)
+task_1.solve_quadratic(1, 2, 1)
 print()
